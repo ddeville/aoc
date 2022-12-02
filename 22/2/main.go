da46b8e1755c2e44b6006dc66e63faaef2781f4c
Damien Deville <damien@ddeville.me>
2022-12-02 15:13:21 -0800

    Implement second part of #2

--- 22/2/main.go
+++ 22/2/main.go
@@ -16,50 +16,95 @@ func main() {
 	data, err := os.ReadFile("input.txt")
 	check(err)
 
-	res := 0
+	res_1 := 0
+	res_2 := 0
 	for _, line := range strings.Split(string(data), "\n") {
 		if len(line) > 0 {
 			vals := strings.Split(line, " ")
-			opponent, mine := vals[0], vals[1]
 
-			// A for Rock, B for Paper, and C for Scissors
-			// X for Rock, Y for Paper, and Z for Scissors
-			// 1 for Rock, 2 for Paper, and 3 for Scissors
-			// 0 if you lost, 3 if the round was a draw, and 6 if you won
+			// Meaning:
+			//  A for Rock, B for Paper, and C for Scissors
+			//  X for Rock, Y for Paper, and Z for Scissors
+			// Points:
+			//	1 for Rock, 2 for Paper, and 3 for Scissors
+			//	0 if you lost, 3 if the round was a draw, and 6 if you won
+			opponent, mine := vals[0], vals[1]
 
 			switch mine {
 			case "X":
-				res += 1
+				res_1 += 1
 				switch opponent {
 				case "A":
-					res += 3
+					res_1 += 3
 				case "B":
-					res += 0
+					res_1 += 0
 				case "C":
-					res += 6
+					res_1 += 6
 				}
 			case "Y":
-				res += 2
+				res_1 += 2
 				switch opponent {
 				case "A":
-					res += 6
+					res_1 += 6
 				case "B":
-					res += 3
+					res_1 += 3
 				case "C":
-					res += 0
+					res_1 += 0
 				}
 			case "Z":
-				res += 3
+				res_1 += 3
 				switch opponent {
 				case "A":
-					res += 0
+					res_1 += 0
 				case "B":
-					res += 6
+					res_1 += 6
 				case "C":
-					res += 3
+					res_1 += 3
+				}
+			}
+
+			// Meaning:
+			//  A for Rock, B for Paper, and C for Scissors
+			//  X to lose, Y to draw, and Z to win
+			// Points:
+			//  1 for Rock, 2 for Paper, and 3 for Scissors
+			//  0 if you lost, 3 if the round was a draw, and 6 if you won
+			opponent, strategy := vals[0], vals[1]
+
+			switch strategy {
+			case "X":
+				res_2 += 0
+				switch opponent {
+				case "A":
+					res_2 += 3
+				case "B":
+					res_2 += 1
+				case "C":
+					res_2 += 2
+				}
+			case "Y":
+				res_2 += 3
+				switch opponent {
+				case "A":
+					res_2 += 1
+				case "B":
+					res_2 += 2
+				case "C":
+					res_2 += 3
+				}
+			case "Z":
+				res_2 += 6
+				switch opponent {
+				case "A":
+					res_2 += 2
+				case "B":
+					res_2 += 3
+				case "C":
+					res_2 += 1
 				}
 			}
 		}
 	}
-	fmt.Println(res)
+	fmt.Println(res_1)
+	fmt.Println(res_2)
 }
